cc08471b92e8bb5167a7bba09f0e8bd44b6dc02b
Michele Mastrogiovanni <michele.mastrogiovanni@gmail.com>
2021-10-10 18:22:40 +0200

    added mod

---
+++ init.lua
@@ -0,0 +1,38 @@
+print("This file will be run at load time!")
+
+minetest.register_node("paolo:mattonella", {
+    description = "Mattonella",
+    tiles = {"mattonella3.jpg"},
+    groups = {cracky = 1}
+})
+
+minetest.register_node("paolo:cielo", {
+    description = "Cielo",
+    tiles = {"sky.jpg"},
+    groups = {cracky = 1}
+})
+
+minetest.register_node("paolo:iota", {
+    description = "Iota",
+    tiles = {"iota.jpg"},
+    groups = {cracky = 1}
+})
+
+minetest.register_node("paolo:tappeto", {
+    description = "Tappeto",
+    tiles = {"tappeto.png"},
+    groups = {cracky = 1}
+})
+
+minetest.register_node("paolo:fiore", {
+    description = "Fiore",
+    tiles = {"fiore.jpg"},
+    groups = {cracky = 1}
+})
+
+minetest.register_node("paolo:miriam", {
+    description = "Miriam",
+    tiles = {"miriam.png"},
+    groups = {cracky = 1}
+})
+
